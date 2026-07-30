4eb86abebc9319932304eeeb6345111bc7e7adf2
james <jb.e.salazar@gmail.com>
2026-07-30 12:04:14 +0200

    Update AKS/ACR role assignment and add infra pipeline workflows

--- infra-terraform/acr.tf
+++ infra-terraform/acr.tf
@@ -7,3 +7,14 @@ resource "azurerm_container_registry" "acr" {
 
   tags = var.tags
 }
+# Grant AKS kubelet identity the AcrPull role on the registry so nodes can pull images
+resource "azurerm_role_assignment" "acr_pull_for_aks" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks,
+    azurerm_container_registry.acr,
+  ]
+}
