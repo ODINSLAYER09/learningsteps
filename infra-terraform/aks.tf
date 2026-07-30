bdf72f5d6814685ea66c66cdbdab811a625f6749
james <jb.e.salazar@gmail.com>
2026-07-30 14:32:26 +0200

    Commit updated AKS Terraform configuration

--- infra-terraform/aks.tf
+++ infra-terraform/aks.tf
@@ -40,8 +40,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Terraform equivalent of: az aks update --attach-acr
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.main.id
+  scope                            = azurerm_container_registry.aks.id
   skip_service_principal_aad_check = true # avoids AAD replication race
 }
